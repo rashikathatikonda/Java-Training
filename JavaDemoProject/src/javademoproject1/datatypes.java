b8e175300859831c66549f245d2faf65fa010dd2
Rashika <trashika@teksystems.com>
2023-02-24 11:17:13 +0530

    push

---
+++ JavaDemoProject/src/javademoproject1/datatypes.java
@@ -0,0 +1,23 @@
+package javademoproject1;
+
+public class datatypes
+{
+	 static byte a=50;
+	 public static void main(String[] args)
+	   {
+		   System.out.println(a);
+		   a=(byte)150;
+		   System.out.println(a);
+		   short b=150;
+		   int c=1000;
+		   System.out.println(b);
+		   System.out.println(Byte.MAX_VALUE);
+		   System.out.println(Short.MAX_VALUE);
+		   System.out.println(Short.MIN_VALUE);
+		   System.out.println(Integer.MAX_VALUE);
+		   float f=1.23f;
+		   System.out.println(f);
+		   double d=12345.6789;
+		   System.out.println(d);
+	   }
+}
