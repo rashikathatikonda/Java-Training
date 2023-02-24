cdd5bccb479c23134f30319b3ed24257403c60bd
Rashika <trashika@teksystems.com>
2023-02-24 11:53:28 +0530

    push

---
+++ JavaDemoProject/src/javademoproject1/operators.java
@@ -0,0 +1,14 @@
+package javademoproject1;
+
+public class operators 
+{
+	public static void main(String[] args)
+	{
+		int a=10,b=5;
+		System.out.println(a+b);//15
+		System.out.println(a-b);//5  
+		System.out.println(a*b);//50  
+		System.out.println(a/b);//2  
+		System.out.println(a%b);//0  
+	}
+}
